7e46404c36cdbad7451579d4ba2d36601b0883b1
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-08-21 22:47:43 +0100

    chore: sorted into methods for DRY

--- main.py
+++ main.py
@@ -2,35 +2,30 @@ import sys
 from logic import process
 
 
+def sort_line(line):
+    line = line.split(" ")
+    command = line[0]
+    username = line[1]
+    amount = line[-1]
+
+    if command == 'Add':
+        credit_card_no = line[2]
+        process(command, username, amount, credit_card_no)
+    
+    print("{} {} to {}'s account".format(command,amount,username))
+
+
 def _direct_input(s_input):
     # if direct input
     for line in s_input:
-        line = line.split(" ")
-        command = line[0]
-        username = line[1]
-        amount = line[-1]
-        if command == 'Add':
-                credit_card_no = line[2]
-                process(command, username, amount, credit_card_no)
-        
-        print("{} {} to {}'s account".format(command,amount,username))
+        sort_line(line)
 
 
 def _file(filename):
     # if file name
     with open(filename, 'r') as file:
         for line in file:
-            line = line.split(" ")
-            command = line[0]
-            username = line[1]
-            amount = line[-1]
-
-            if command == 'Add':
-                credit_card_no = line[2]
-                process(command, username, amount, credit_card_no)
-            
-            process(command, username, amount)
-            print("{} {} to {}'s account".format(command,amount,username))
+            sort_line(line)
 
 
 if __name__ == "__main__":
