983f10bcacf90f4645affc192ada4642752a7681
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-08-21 22:42:26 +0100

    feature: created base for stdin input

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import sys
 from logic import process
-# if direct input
 
-# if file name
-filename = sys.argv[1]
-with open(filename, 'r') as file:
-    for line in file:
+
+def _direct_input(s_input):
+    # if direct input
+    for line in s_input:
         line = line.split(" ")
         command = line[0]
         username = line[1]
         amount = line[-1]
-
         if command == 'Add':
-            credit_card_no = line[2]
-            process(command, username, amount, credit_card_no)
+                credit_card_no = line[2]
+                process(command, username, amount, credit_card_no)
         
-        process(command, username, amount)
-        # print("{} {} to {}'s account".format(command,amount,username))
+        print("{} {} to {}'s account".format(command,amount,username))
+
+
+def _file(filename):
+    # if file name
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.split(" ")
+            command = line[0]
+            username = line[1]
+            amount = line[-1]
+
+            if command == 'Add':
+                credit_card_no = line[2]
+                process(command, username, amount, credit_card_no)
+            
+            process(command, username, amount)
+            print("{} {} to {}'s account".format(command,amount,username))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+       filename = sys.argv[1]
+       _file(filename)
+    else:
+        _direct_input(sys.stdin)
+
