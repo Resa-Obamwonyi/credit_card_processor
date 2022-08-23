5f52df27bdcd538b89010840e445645f857c2ee0
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-08-23 23:20:53 +0100

    chore: clean up methods and logic checks

--- database/db.py
+++ database/db.py
@@ -20,27 +20,44 @@ class AccountDB():
         is_valid = luhn_validation(number)
         return is_valid
 
+    def validate_balance(self, balance):
+        return True if type(balance) == int else False
+
+
     def validate_acc_limit(self, key, amount):
         """ Validates that a new charge transaction does not exceed account limit """
         limit = self._db.get(key)["limit"]
         balance = self._db.get(key)["balance"]
 
-        if (amount + balance) > limit:
-            return False
-        else:
-            return True
+        #check that balance is valid type
+        is_balance_valid = self.validate_balance(balance)
+        if is_balance_valid:
+            if (amount + balance) <= limit:
+                return True
+        return False
     
     def make_charge_transaction(self, name, amount):
-        """ Adds money to an account, ignore if it raises above limit, ignores if card is invalid """
+        """ Adds money to an account, ignore if it raises above limit, ignore if card is invalid """
         account = self.get_account(name)
         if account:
-            is_charge_valid = self.validate_acc_limit(name, amount)
-            if is_charge_valid:
-                account["balance"] = account["balance"] + amount
+            balance = account["balance"]
+            #check that balance is valid type
+            is_balance_valid = self.validate_balance(balance)
+            if is_balance_valid:
+                is_charge_valid = self.validate_acc_limit(name, amount)
+                if is_charge_valid:
+                    account["balance"] = balance + amount
 
     def make_credit_transaction(self, name, amount):
-        """ Removes money from an account, balance can be negative, ignores if card is invalid """
-        pass
+        """ Removes money from an account, balance can be negative, ignore if card is invalid """
+        account = self.get_account(name)
+        if account:
+            balance = account["balance"]
+            # check that balance is valid type
+            is_balance_valid = self.validate_balance(balance)
+            if is_balance_valid:
+                account["balance"] = balance - amount
+
 
     def generate_account_statement(self):
         for account in self._db:
