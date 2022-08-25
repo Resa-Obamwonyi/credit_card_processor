50adac64588cbf31abe22add4a2b9fcfdb00d09c
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-08-25 21:52:58 +0100

    chore: completed tests and modified readme

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 This program will add new credit card accounts, process charges and credits against them, and display summary information.
 
 ## Design Descisions
-- A major design principle I attemoted to communicate in this task is that "simple is better". 
+- A major design principle I attempted to communicate in this task is that "simple is better". I understood that since this was a standard read and write of input, there was no need for an actual database for long term storage. 
+- I proceeded to mimic a database model using python, this kept my logic simple, precise and clear.
+- 
 ### Technologies Used
-- Python 3
-- Unittest Module (inbuilt in Python 3)
+- Python 3 and a few of its inbuilt libraries.
 
-### How to Run the programme
+### How to Run the program
 #### Test with txt file
-
+- To run the program with a txt file, in the root of the working directory, run `python3 main.py file_name.txt` (please ensure your file is in the root directory also, or specify the path e.g `python3 main.py  path_to_file/file_name.txt`), there is also a test txt file available named `data.txt`, you can run `python3 main.py data.txt` to test with that.
 #### Test with input
+- To run the program with direct input, run `python3 main.py` and paste or write all your transactions and then `CTRL-D` (Mac) or `CTRL-Z followed by <return>` (Windows) to signify End of File or Writing. 
 
 ### How to Run tests
-- To run autorun tests run `python -m unittest discover` or `python -m unittest test_program.py`
+- To run tests use the command `python3 -m unittest discover` or `python3 -m unittest test_program.py`
--- test_program.py
+++ test_program.py
@@ -1,22 +1,29 @@
+from io import StringIO
 import unittest
 import main
+from unittest.mock import patch
 from utils.logic import luhn_validation
 
 class TestProgram(unittest.TestCase):
     def test_read_file(self):
-        self.assertEqual(main.read_file('data.txt'), 
-            "Lisa: $-93 \n Quincy: error \n Tom: $500")
+        expected_output = "Lisa: $-93\nQuincy: error\nTom: $500\n"
+        with patch('sys.stdout', new = StringIO()) as fake_output:
+            main.read_file('data.txt')
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
     def test_read_direct_input(self):
-        self.assertEqual(main.read_direct_input(["Add Tom 4111111111111111 $1000",
+        expected_output = "\n\nLisa: $-93\nQuincy: error\nTom: $500\n"
+        with patch('sys.stdout', new = StringIO()) as fake_output:
+            main.read_direct_input(["Add Tom 4111111111111111 $1000",
                                                 "Add Lisa 5454545454545454 $3000",
                                                 "Add Quincy 1234567890123456 $2000",
                                                 "Charge Tom $500",
                                                 "Charge Tom $800",
                                                 "Charge Lisa $7",
                                                 "Credit Lisa $100",
-                                                "Credit Quincy $200"]), 
-            "Lisa: $-93 \n Quincy: error \n Tom: $500")
+                                                "Credit Quincy $200"])
+
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
     def test_luhn_validation(self):
         self.assertTrue(luhn_validation("4111111111111111"))
